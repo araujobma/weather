b2019e36448b95ebd8c6c6d030b608530b4b6ce5
Bruno M A Araujo <araujobma@gmail.com>
2024-07-21 22:09:35 -0300

    feat: uses postgresql backend

--- worker.py
+++ worker.py
@@ -8,10 +8,10 @@ celery_app = Celery(__name__)
 celery_app.conf.broker_url = os.environ.get(
     "CELERY_BROKER_URL", "amqp://user:mypass@rabbitmq:5672//"
 )
-# celery_app.conf.result_backend = os.environ.get(
-#     "CELERY_RESULT_BACKEND",
-#     "db+postgresql://myuser:mypassword@localhost:5432/mydatabase",
-# )
+celery_app.conf.result_backend = os.environ.get(
+    "CELERY_RESULT_BACKEND",
+    "db+postgresql://user:mypass@postgres:5432/db",
+)
 
 
 @celery_app.task(name="create_task")
